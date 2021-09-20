33bbc616d71e32b60547dd976460515dbcdf9464
[Cedric] <[0320987@student.rocvantwente.nl]>
2021-09-20 14:33:05 +0200

    database en account functionaliteit

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using CRYBZ_CCSB.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRYBZ_CCSB.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public AccountController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
