0b5f8899eb189e1ec0d6a06a732ac5e3a14fdf60
[Cedric] <[0320987@student.rocvantwente.nl]>
2021-09-20 13:18:48 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CRYBZ_CCSB.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
