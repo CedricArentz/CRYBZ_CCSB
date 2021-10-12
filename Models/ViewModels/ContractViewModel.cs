7011907d424d4a0f410dc00e46fcc94985e48e05
Ronan Haveman <55980473+Rhaveman@users.noreply.github.com>
2021-10-12 10:28:11 +0200

    I can see the page now!

---
+++ Models/ViewModels/ContractViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRYBZ_CCSB.Models.ViewModels
+{
+    public class ContractViewModel
+    {
+        public int ContractID { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+    }
+}
