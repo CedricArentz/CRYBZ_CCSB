ce0f523c880622dece5073984f664e5b8181332e
Ronan Haveman <55980473+Rhaveman@users.noreply.github.com>
2021-10-12 09:36:07 +0200

    Model File

---
+++ Models/Contract.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRYBZ_CCSB.Models
+{
+    //Datatabel is declared here for Contract
+    public class Contract
+    {
+        //ContractID is a primary key
+        public int ContractID { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+    }
+}
